c6791eb0fe2341900d4cc0d14881390148493635
mtmiezan <miezankone@gmail.com>
2024-07-02 16:34:02 -0400

    Save user expereince

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,8 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experience').value;
+
     alert('Thank you for your valuable feedback')
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -14,6 +16,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
     document.getElementById('userInfo').style.display = 'block';
 }
 const submitButton=document.getElementById('submitBtn');
