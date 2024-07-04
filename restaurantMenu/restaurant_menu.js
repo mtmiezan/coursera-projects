536970da4df6b91225b6c127f110561425a3849b
mtmiezan <miezankone@gmail.com>
2024-07-04 18:12:33 -0400

    Generate menu restaurant

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,19 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+//Generate breakfast menu
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// Generate main course menu
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+//Generate dessert menu
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
